1ec72f1d0f848de2afa91af8ad96bf6cb4c9496f
Rodrigo Clemente <rodrigom.clemente@hotmail.com>
2020-10-29 01:19:00 -0300

    feat: Added exception middleware and SummaryViewComponent
    An example for a Global exception handler middleware and a custom view component to show model validation messages.

---
+++ src/RazorPagesSimpleLogin/Extensions/ExceptionMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesSimpleLogin.Extensions
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                httpContext.Response.StatusCode = 500;
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
---
+++ src/RazorPagesSimpleLogin/Extensions/SummaryViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesSimpleLogin.Extensions
+{
+    public class SummaryViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var i = 123;
+            return View();
+        }
+    }
+}
